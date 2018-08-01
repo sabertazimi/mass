5a6d47e6bf14c9f8697502e786333f63044569e4
sabertazimi <sabertazimi@gmail.com>
2018-08-01 21:01:39 +0800

    add(boilerplate): set up boilerplate project
    issue sabertazimi/awesome-notes#54

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mass
+
+[![Build Status](https://travis-ci.org/sabertazimi/mass.svg?branch=master)](https://travis-ci.org/sabertazimi/mass)
+
 A minimal sass-based UI library
+
+## Structure
+
+- `build`: for `exmaples` building result
+- `lib`: for `src` building result (publish to npm)
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    main: './src/mass.scss',
+  },
+  output: {
+    filename: devMode ? '[name].js' : '[name].min.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin('lib'),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].min.css',
+      chunkFilename: devMode ? '[id].css' : '[id].min.css',
+    }),
+    new StyleLintPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
